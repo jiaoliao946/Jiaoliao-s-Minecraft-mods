72aa3c221570c307182e8c61784e900868e55deb
jiaoliao946 <1686950986@qq.com>
2023-10-10 22:01:58 +0800

    Add first item

--- src/main/java/jiaoliao/ocean12/item/ItemRegistryHandler.java
+++ src/main/java/jiaoliao/ocean12/item/ItemRegistryHandler.java
@@ -1,17 +1,17 @@
 package jiaoliao.ocean12.item;
 
 import net.minecraft.item.Item;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.event.RegistryEvent.Register;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.registries.IForgeRegistry;
 
-@Mod.EventBusSubscriber
+@EventBusSubscriber
 public class ItemRegistryHandler
 {
     public static final ItemClamShell CLAM_SHELL = new ItemClamShell();
     @SubscribeEvent
-    public static void onRegistry(RegistryEvent.Register<Item> event)
+    public static void onRegistry(Register<Item> event)
     {
         IForgeRegistry<Item> registry = event.getRegistry();
         registry.register(CLAM_SHELL);
