2c18d5bc9da48abe63f3ea2e8d3b52ba776ab73f
jiaoliao946 <1686950986@qq.com>
2023-10-12 20:51:13 +0800

    Add first item

--- src/main/java/jiaoliao/ocean12/item/ItemRegistryHandler.java
+++ src/main/java/jiaoliao/ocean12/item/ItemRegistryHandler.java
@@ -1,9 +1,14 @@
 package jiaoliao.ocean12.item;
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent.Register;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.registries.IForgeRegistry;
 
 @EventBusSubscriber
@@ -15,5 +20,11 @@ public class ItemRegistryHandler
     {
         IForgeRegistry<Item> registry = event.getRegistry();
         registry.register(CLAM_SHELL);
+    }
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public static void onModelRegistry(ModelRegistryEvent event)
+    {
+        ModelLoader.setCustomModelResourceLocation(CLAM_SHELL, 0, new ModelResourceLocation(CLAM_SHELL.getRegistryName(), "inventory"));
     }
 }
