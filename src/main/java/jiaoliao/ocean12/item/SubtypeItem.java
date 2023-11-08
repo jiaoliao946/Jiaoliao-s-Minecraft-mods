d04ce5d7c452e4fcd846fedc1e43327af1f079d1
jiaoliao946 <1686950986@qq.com>
2023-11-08 15:22:32 +0800

    Rewrite getSubItems to simplify code

--- src/main/java/jiaoliao/ocean12/item/SubtypeItem.java
+++ src/main/java/jiaoliao/ocean12/item/SubtypeItem.java
@@ -2,10 +2,16 @@ package jiaoliao.ocean12.item;
 
 import jiaoliao.ocean12.Ocean12;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
+
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Objects;
 
 public class SubtypeItem extends Item     //The item that has subtype
 {
@@ -17,7 +23,7 @@ public class SubtypeItem extends Item     //The item that has subtype
     }
     public ResourceLocation getSubtypeName(String meta)
     {
-        return new ResourceLocation(this.getRegistryName().getResourceDomain(),
+        return new ResourceLocation(Objects.requireNonNull(this.getRegistryName()).getResourceDomain(),
                 this.getRegistryName().getResourcePath() + meta);
     }
     public void setSubtypeResourceLocation(int maxData)     //Set resource location to each subtype
@@ -33,30 +39,34 @@ public class SubtypeItem extends Item     //The item that has subtype
 
 class ItemClamShell extends SubtypeItem     //Clam-Shell
 {
-    public ItemStack blue, green, yellow, red;
     public ItemClamShell()
     {
-        this.blue = new ItemStack(this, 1, 0);
-        this.green = new ItemStack(this, 1, 1);
-        this.yellow = new ItemStack(this, 1, 2);
-        this.red = new ItemStack(this, 1, 3);     //Set item-stack by metadata
         this.setUnlocalizedName(Ocean12.MODID + ".clamShell");
         this.setRegistryName("clam_shell");     //Set registry name
         this.setMaxStackSize(64);     //Set max stack size
     }
+    @Override     //Overwrite get subtype item function
+    @ParametersAreNonnullByDefault     //Parameters are nullable
+    public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items)
+    {
+        if(this.isInCreativeTab(tab))
+        {
+            for(int i = 0; i < 4; ++i)
+            {
+                items.add(new ItemStack(this, 1, i));
+            }
+        }
+    }
     @Override     //Overwrite get unlocalized name function
+    @Nonnull     //Can't return null
     public String getUnlocalizedName(ItemStack stack)
     {
         switch (stack.getMetadata())
         {
-            case 0 :
-                return this.getUnlocalizedName() + ".blue";
-            case 1 :
-                return this.getUnlocalizedName() + ".green";
-            case 2 :
-                return this.getUnlocalizedName() + ".yellow";
-            default:
-                return this.getUnlocalizedName() + ".red";     //Set different unlocalized name for different color
+            case 0 : return this.getUnlocalizedName() + ".blue";
+            case 1 : return this.getUnlocalizedName() + ".green";
+            case 2 : return this.getUnlocalizedName() + ".yellow";
+            default : return this.getUnlocalizedName() + ".red";     //Set different unlocalized name for different color
         }
     }
 }
